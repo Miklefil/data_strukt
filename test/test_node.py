58dcc0855876cd447c716b0e73bfac21643a442e
twist <twistysmig@mail.ru>
2023-03-09 22:40:39 +0300

    task3 test add and passed

--- test/test_node.py
+++ test/test_node.py
@@ -1,8 +1,10 @@
 import unittest
 #import pytest
-from utils.node import Node
+from utils.node import Node, Queue
+
 
 class Test_Node(unittest.TestCase):
+
     def SetUp(self):
         self.node = Node()
         self.node_10 = Node(10)
@@ -18,3 +20,18 @@ class Test_Node(unittest.TestCase):
         self.node_10 = Node(10)
         self.node_20 = Node(20, self.node_10)
         self.assertEqual(self.node_20.next_node, self.node_10)
+
+
+class TestQueue(unittest.TestCase):
+
+    def test_enqueue(self):
+        queue = Queue()
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        queue.enqueue('data3')
+        self.assertEqual(queue.top.data, 'data1')
+        self.assertEqual(queue.top.next_node.data, 'data2')
+        self.assertEqual(queue.tail.data, 'data3')
+        self.assertEqual(queue.tail.next_node, None)
+        with self.assertRaises(AttributeError):
+            queue.tail.next_node.data()
