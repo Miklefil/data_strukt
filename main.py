a19dd85842707d64dac17f24a569ea8a8a5ea132
twist <twistysmig@mail.ru>
2023-03-14 16:14:57 +0300

    task5 linked list add func

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 from utils.stack import Stack
 from utils.node import Node, Queue
 
-queue = Queue()
-queue.enqueue('data1')
-queue.enqueue('data2')
-queue.enqueue('data3')
-print(queue.dequeue())
 
-print(queue.dequeue())
-
-print(queue.dequeue())
-
-print(queue.dequeue())
 
 
 
