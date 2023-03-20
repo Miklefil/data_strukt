c1d31bde25d33d7f3611dc2deea036c4ee084617
twist <twistysmig@mail.ru>
2023-03-20 15:09:18 +0300

    task six сделано тесты ок

--- test/test_linked_list.py
+++ test/test_linked_list.py
@@ -10,6 +10,7 @@ class TestNode(unittest.TestCase):
 
 
 class TestLinkedList(unittest.TestCase):
+
     def test_insert_beginning(self):
         ll = LinkedList()
         ll.insert_beginning({'id': 1})
@@ -21,3 +22,21 @@ class TestLinkedList(unittest.TestCase):
         ll.insert_at_end({'id': 2})
         ll.insert_at_end({'id': 3})
         self.assertEqual(ll.print_ll(), " {'id': 2} -> {'id': 3} ->None")
+
+    def test_to_list(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        ll.insert_beginning({'id': 0, 'username': 'serebro'})
+        lst = ll.to_list()
+        self.assertEqual(len(lst), 4)
+        self.assertEqual(lst[0]['id'], 0)
+        self.assertEqual(lst[3]['id'], 3)
+
+    def test_get_data_by_id(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end({'id': 2, 'username': 'mosh_s'})
+        self.assertEqual(ll.get_data_by_id(2), {'id': 2, 'username': 'mosh_s'})
+        self.assertEqual(ll.get_data_by_id(3), None)
